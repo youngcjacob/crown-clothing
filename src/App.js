7f84cf81edcccc54213f81e1cf4b9358e13bf66f
youngcjacob <youngcjacob@gmail.com>
2023-09-12 20:49:46 -0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () => {
   return (
 
     <Directory categories={categories} />
+    
 
     
   );
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -11,7 +11,7 @@ const CategoryItem = ({ category }) => {
       <div className="category-body-container">
         <h2>{title}
         </h2>
-        <p>Shop Now</p>
+        <p>Shop Now!</p>
       </div>
     </div>
 
