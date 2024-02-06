74ab571f4b25178651c866db2b43830cd4a1ee1e
youngcjacob <youngcjacob@gmail.com>
2024-02-05 17:38:12 -0800

    added button styling using Styled Components

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,5 +1,9 @@
-.button-container {
-  min-width: 165px;
+
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+
+min-width: 165px;
   width: auto;
   height: 50px;
   letter-spacing: 0.5px;
@@ -22,18 +26,22 @@
     border: 1px solid black;
   }
 
-  &.google-sign-in {
-    background-color: #4285f4;
-    color: white;
 
-    &:hover {
-      background-color: #357ae8;
-      border: none;
-    }
-  }
+`
 
-  &.inverted {
-    background-color: white;
+export const GoogleSignInButton = styled(BaseButton)`
+
+background-color: #4285f4;
+color: white;
+
+&:hover {
+  background-color: #357ae8;
+  border: none;
+}
+`
+
+export const InvertedButton = styled(BaseButton)`
+background-color: white;
     color: black;
     border: 1px solid black;
 
@@ -42,5 +50,6 @@
       color: white;
       border: none;
     }
-  }
-}
+`
+
+
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 
 import './product-card.styles.scss'
 
-import Button from '../button/button.component'
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component'
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -22,7 +22,7 @@ const ProductCard = ({ product }) => {
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <Button buttonType='inverted' onClick={addProductToCart}>Add to cart</Button>
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>Add to cart</Button>
     </div>
     )
 }
